5c038a852ed653a82317e2cde0d07d246623c5fc
Juhyeong Kang <jhkang@astro.snu.ac.kr>
2016-07-26 21:11:15 +0900

    Python plot
    Include 2d array plot.

--- example_script/plot_test.py
+++ example_script/plot_test.py
@@ -98,3 +98,14 @@ plt.title(r'SDO/AIA 171 $\mathbf{\AA}$',fontsize=20,fontweight='bold')
 #show the image---------------------------------------------
 plt.show()
 
+#2d image--------------------------------------------------
+a=np.arange(25)
+b=a.reshape((5,5))-12
+fig=plt.figure(2,figsize=(16,10))
+im=plt.imshow(b,cmap=plt.cm.Greys_r,origin='lower')
+plt.clim(-10,10)   # color bar limit
+plt.xticks([0,2,4],fontsize=20)
+plt.colorbar(im,ticks=[-10,0,10])
+plt.show()
+#You have to causion that python plot 2d array from left top to right bottom
+#But IDL plot 2d array from left bottom to top right
